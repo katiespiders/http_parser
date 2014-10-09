4158ebd60768e7db5dbb33de8c05746f267b15d7
Katie <kab761@gmail.com>
2014-10-09 09:38:40 -0700

    platinum

--- http.rb
+++ http.rb
@@ -1,49 +1,75 @@
-class Http
+class HttpParser
+  attr_accessor :method, :path, :elements_hash
 
   def bronze(string)
-    elements_hash = {}
+    @elements_hash = {}
     elements = string.split("&")
     elements.each do |element|
       key, value = element.split("=")
       m = /\[.+\]/.match(key)
       if m
         outer_key = m.pre_match
-        # if m.to_s[1...-1].index("[]")
-        #   gold(m)
-        # end
-        inner_key = m.to_s[1...-1]
-        # if inner_key.index("[]")
-        #   gold(inner_key)
-        # end
-        if elements_hash[outer_key]
-          elements_hash[outer_key][inner_key] = value
+        inner_key = m.to_s
+
+        if inner_key.index("[]")
+          gold(inner_key, value)
         else
-          elements_hash[outer_key] = {inner_key => value}
+          inner_key = inner_key.slice(1...-1)
+          if @elements_hash[outer_key]
+            @elements_hash[outer_key][inner_key] = value
+          else
+            @elements_hash[outer_key] = {inner_key => value}
+          end
         end
+
       else
-        elements_hash[key] = value
+        @elements_hash[key] = value
       end
     end
-    elements_hash
+    @elements_hash
   end
 
   def silver(string)
     bronze(string.gsub("%20", " "))
   end
 
-  def gold(string)
-    puts string
+  def gold(string, value)
+    m = /\[\]/.match(string)
+    outer_key = m.pre_match.slice(1...-1)
+    inner_key = m.post_match.slice(1...-1)
+    if @elements_hash[outer_key]
+      @elements_hash[outer_key] << {inner_key => value}
+    else
+      @elements_hash[outer_key] = [{inner_key => value}]
+    end
+  end
+
+  def platinum(string)
+    m = /\n\n/.match(string)
+    @elements_hash = silver(m.post_match)
+    @method = /\ \//.match(m.pre_match).pre_match
+    @path = /\/\w*/.match(m.pre_match)
   end
 end
 
-bronze_test_string = "market[name]=Cap Hill Market&market[description]=A good place to buy produce"
 
-silver_test_string = "market[name]=Cap%20Hill%20Market&market[description]=A%20good%20place%20to%20buy%20produce"
+#
+# bronze_test_string = "market[name]=Cap Hill Market&market[description]=A good place to buy produce"
+#
+# silver_test_string = "market[name]=Cap%20Hill%20Market&market[description]=A%20good%20place%20to%20buy%20produce"
 
 # gold_test_string = "market[name]=Cap%20Hill%20Market&market[description]=A%20good%20place%20to%20buy%20produce&market[vendors_attributes][][name]=Jules%20Produce"
 
+platinum_test_string = <<-EOS
+GET /markets HTTP/1.1
+User-Agent: curl/7.30.0
+Host: localhost:3000
+Content-Length: 141
+Accept: */*
 
-parser = Http.new
-puts parser.bronze(bronze_test_string)
-puts parser.silver(silver_test_string)
-# puts parser.silver(gold_test_string)
+market[name]=Cap%20Hill%20Market&market[description]=A%20good%20place%20to%20buy%20produce&market[vendors_attributes][][name]=Jules%20Produce
+EOS
+
+parser = HttpParser.new
+parser.platinum(platinum_test_string)
+puts parser.method, parser.path, parser.elements_hash
